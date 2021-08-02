e824411f2ac56b44f397fbe928824de1eedece94
Alexander Lukyanov <lux-nova@mail.ru>
2021-08-02 17:33:05 +0300

    local init

---
+++ src/main/java/haplo/Haplogroup.java
@@ -0,0 +1,11 @@
+package haplo;
+
+import java.util.LinkedList;
+
+public class Haplogroup {
+    private String notation;
+    private Haplotype reference;
+
+    private Haplogroup ancestor;
+
+}
---
+++ src/main/java/haplo/Haplotype.java
@@ -0,0 +1,8 @@
+package haplo;
+
+import java.util.LinkedList;
+
+public class Haplotype {
+    private LinkedList<String> mutations;
+    private Haplogroup group;
+}
